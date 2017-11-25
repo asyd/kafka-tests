5567d03be7e9238679072b44557ab5e2a6d2ea0e
Bruno Bonfils <asyd@asyd.net>
2017-11-26 00:23:57 +0100

    WIP

--- procuder.py
+++ procuder.py
@@ -1,9 +1,10 @@
 import asyncio
 from aiokafka import AIOKafkaProducer
 from common import config_load
+import datetime
 
 
-async def consumer():
+async def producer():
     config = config_load()
     kafka_producer = AIOKafkaProducer(
         loop=loop,
@@ -11,9 +12,15 @@ async def consumer():
     )
     await kafka_producer.start()
     try:
+        count = 0
         while True:
+            count += 1
             await kafka_producer.send_and_wait(config['kafka']['topic'],
-                                               b"Hello world")
+                                               value=b"Hello world",
+                                               # key=b",".join([count, datetime.datetime.now()]),
+                                               # value=b"Hello world {}".__format__(datetime.datetime.now)
+                                               )
+            print("Send messaged: {}".format(count))
             await asyncio.sleep(config['kafka']['producer']['sleep'])
     finally:
         await kafka_producer.stop()
@@ -21,4 +28,4 @@ async def consumer():
 
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(consumer())
+    loop.run_until_complete(producer())
